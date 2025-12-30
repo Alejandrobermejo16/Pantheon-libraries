f5a0686ca577063fa439303721fa6d1645ee4c0b
Alejandro <alejandrobermejomendez170712@gmail.com>
2025-12-30 12:03:08 +0100

    feat(app-component): add menu icon float in grid-panel-header

--- projects/pantheon-ui/src/lib/pantheon-Panels/grid-panel/grid-pannel-header/grid-panel-header.component.ts
+++ projects/pantheon-ui/src/lib/pantheon-Panels/grid-panel/grid-pannel-header/grid-panel-header.component.ts
@@ -10,8 +10,16 @@ import { CommonModule } from '@angular/common';
 })
 export class GridPanelHeaderComponent {
 
-  
+
   @Input() columns: Array<String> = [];
+  @Input() menuAux: boolean = false;
+  @Input() iconMenu: string = 'menu';
+  /**
+   * Icono que se mostrará en el botón del menú auxiliar.
+   * Debe ser un nombre válido de Material Icons (Filled) disponible en:
+   * https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Filled
+   * Ejemplos válidos: 'menu', 'more_vert', 'add', 'account_circle', etc.
+   */
   gridTemplateColumns: string = `repeat(${this.columns.length}, 1fr)`;
 
 }
